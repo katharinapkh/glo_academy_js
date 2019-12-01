2d5386e7374a4e1a875c7855742f23a5fa936407
Kateryna Panchenko <buddhagolden@gmail.com>
2019-12-02 00:47:12 +0200

    lesson-two done

--- lesson_first/script.js
+++ lesson_first/script.js
@@ -1,4 +1,17 @@
-let money = 3000, income, addExpenses, deposit, mission, period;
+let money = +prompt('Ваш месячный доход?'),
+    income = 'иллюстрации',
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    mission = 20000,
+    period = 12;
 
-alert('Приложение для расчета бюджета');
-console.log(money);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
+console.log(`Период ${period} месяц(ев). Цель заработать ${mission} долларов.`);
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
+console.log(money % 30);
